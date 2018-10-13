ab34b938a6957ee4cc8d1b919919e0aa1c2e1914
irvinghu07 <rhu5@dons.usfca.edu>
2018-10-12 20:43:37 -0700

    RandomGenerator.java completed, working on hybridSort

--- src/Driver.java
+++ src/Driver.java
@@ -1,10 +1,13 @@
+import java.util.Arrays;
+
 public class Driver {
     public static void main(String[] args) {
         String[] arg = {"8"};
         int running_size = 16;
+        System.out.println(Arrays.toString(RandomGenerator.generate()));
         try {
-            running_size = ArgumentParser.parseSize(arg);
-            System.out.println("entered running_size: " + running_size);
+//            running_size = ArgumentParser.parseSize(arg);
+//            System.out.println("entered running_size: " + running_size);
         } catch (Exception e) {
             System.out.println("========program shutting down========");
             System.out.println(e.getMessage());
---
+++ src/RandomGenerator.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+public class RandomGenerator {
+
+    public static int[] generate() {
+        return generate(100);
+    }
+
+    public static int[] generate(int arraySize) {
+        return generate(arraySize, 100);
+    }
+
+    public static int[] generate(int arraySize, int maximum) {
+        int[] out = new int[arraySize];
+        ArrayList<Integer> numbers = new ArrayList<>();
+        while (numbers.size() != arraySize) {
+            numbers.add(new Random().nextInt(maximum));
+        }
+        for (int i = 0; i < numbers.size(); i++) {
+            out[i] = numbers.get(i);
+        }
+        return out;
+    }
+}
