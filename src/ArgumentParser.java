7a5f17a19905dad747fc16f60990d35664903509
irvinghu07 <rhu5@dons.usfca.edu>
2018-10-12 20:11:41 -0700

    solving conflict

--- src/ArgumentParser.java
+++ src/ArgumentParser.java
@@ -1,2 +1,14 @@
 public class ArgumentParser {
+    public static final String PATTERN = "\\d";
+    public static Integer parseSize(String[] args) {
+        if (args.length!= 1){
+            throw new RuntimeException("invalid input of argument: enter one integer as input size");
+        }
+        String content = args[0];
+        if (!content.matches(PATTERN)){
+            throw new RuntimeException("invalid input of argument: entered not a number");
+        }else{
+            return Integer.valueOf(content);
+        }
+    }
 }
--- src/Driver.java
+++ src/Driver.java
@@ -1,2 +1,6 @@
 public class Driver {
+    public static void main(String[] args) {
+        String[] arg = {"1"};
+        System.out.println(ArgumentParser.parseSize(arg));
+    }
 }
