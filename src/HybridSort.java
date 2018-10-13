455f1f4b19a7fce2fea96521ddc39a27d904339e
irvinghu07 <rhu5@dons.usfca.edu>
2018-10-12 22:50:11 -0700

    working on HybridSort

--- src/HybridSort.java
+++ src/HybridSort.java
@@ -6,36 +6,49 @@ public class HybridSort extends AbstractSortingImpl {
 
     public static TreeMap<Integer, Integer> runs;
 
+    public static TreeMap<Integer, Integer> uruns;
+
     public HybridSort(int running_size) {
         RUNNING_SIZE = running_size;
         runs = new TreeMap<>();
+        uruns = new TreeMap<>();
     }
 
     public HybridSort() {
         RUNNING_SIZE = 16;
         runs = new TreeMap<>();
+        uruns = new TreeMap<>();
     }
 
     @Override
     public void sort(int[] a) {
         findRuns(a);
-//        runs.forEach((key, value) -> System.out.println(String.format("start:%d\tvalue:%d", key, value)));
-        for (Integer start : runs.keySet()) {
-            Integer end = runs.get(start);
-            System.out.println(String.format("start:%d\tend:%d", start, end));
-            System.out.println("============================");
-            for (int i = start; i <= end; i++) {
-                System.out.print(a[i] + ",");
+        printRuns(a);
+        findUnsortedPart(a);
+        printURuns(a);
+    }
 
+
+
+    private void findUnsortedPart(int[] a) {
+        int uStart = 0;
+        for (Integer start : runs.keySet()) {
+            if (start == 0) {
+                uStart = runs.get(start) + 1;
+                break;
             }
-            System.out.println();
-            System.out.println("============================");
+            uruns.put(uStart, start - 1);
+            uStart = runs.get(start) + 1;
+        }
+        if (uStart != a.length - 1) {
+            uruns.put(uStart, a.length - 1);
         }
     }
 
+
     private void findRuns(int[] a) {
         int start = -1;
-        int end = -1;
+        int end;
         boolean mayBeARun = false;
         for (int i = 0; i < a.length - 1; i++) {
             if (a[i] <= a[i + 1] && !mayBeARun) {
@@ -52,5 +65,33 @@ public class HybridSort extends AbstractSortingImpl {
         }
     }
 
+
+    private void printRuns(int[] a) {
+        for (Integer start : runs.keySet()) {
+            Integer end = runs.get(start);
+            System.out.println(String.format("start:%d\tend:%d", start, end));
+            System.out.println("============================");
+            for (int i = start; i <= end; i++) {
+                System.out.print(a[i] + ",");
+
+            }
+            System.out.println();
+            System.out.println("============================");
+        }
+    }
+
+    private void printURuns(int[] a) {
+        for (Integer start : uruns.keySet()) {
+            Integer end = uruns.get(start);
+            System.out.println(String.format("start:%d\tend:%d", start, end));
+            System.out.println("============================");
+            for (int i = start; i <= end; i++) {
+                System.out.print(a[i] + ",");
+
+            }
+            System.out.println();
+            System.out.println("============================");
+        }
+    }
 
 }
--- src/SelectionSort.java
+++ src/SelectionSort.java
@@ -1,12 +1,18 @@
 public class SelectionSort extends AbstractSortingImpl {
     @Override
     public void sort(int[] a) {
-//        int insertionIndex = 0;
         int minIndex;
         for (int i = 0; i < a.length; i++) {
             minIndex = findMinAtIndex(a, i, a.length);
             swapValues(a, i, minIndex);
-//            insertionIndex++;
+        }
+    }
+
+    public void sortSpecifiedRegion(int[] a, int start, int end) {
+        int minIndex;
+        for (int i = start; i <= end; i++) {
+            minIndex = findMinAtIndex(a, i, end);
+            swapValues(a, i, minIndex);
         }
     }
 }
