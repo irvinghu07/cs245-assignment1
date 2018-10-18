a472f1e6bfe08f4babce2b7949ed00ae6e8c10cc
irvinghu07 <rhu5@dons.usfca.edu>
2018-10-18 00:00:49 -0700

    Driver.java modified

--- src/Driver.java
+++ src/Driver.java
@@ -2,12 +2,11 @@ import java.util.Arrays;
 
 public class Driver {
     public static void main(String[] args) {
-        String[] arg = {"64"};
         int running_size = 16;
         int[] arr = RandomGenerator.generate();
         System.out.println(Arrays.toString(arr));
         try {
-            running_size = ArgumentParser.parseSize(arg);
+            running_size = ArgumentParser.parseSize(args);
             System.out.println("entered running_size: " + running_size);
         } catch (Exception e) {
             System.out.println("========program shutting down========");
